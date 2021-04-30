70c2501f9249e9c4e892d13d5dc2867a24f14ca0
Shubhankar Mane <onlinestudysam@gmail.com>
2021-04-30 10:12:44 +0530

    Added endpoint to create, update and view customers

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
 const authorize = require("./middleware/authorize");
 const errorHandler = require("./middleware/errorHandler");
-const app = express();
 const db = require("./models");
 const port = process.env.PORT;
+const helmet = require("helmet");
 
 app.use(express.json());
+app.use(helmet());
 
 // Routes go here
 const user = require("./routes/controllers/userController");
+const customers = require("./routes/controllers/customerController");
+const books = require("./routes/controllers/bookController");
+
 // Use routes
 app.use("/api/users", user);
+app.use("/api/customers", customers);
+app.use("/api/books", books);
 
-app.get("/", authorize, userAuth);
-
-function userAuth() {
-  console.log("User authorized!");
-}
+/*
+TODO: Route for adding an author
+TODO: Route for adding a genre
+TODO: Route for the entire CRUD for rentals
+*/
 
 // Global error handler
 app.use(errorHandler);
