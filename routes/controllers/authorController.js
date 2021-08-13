79c0b300c6e13e5b8efdddcf5cfdca943082aa36
shubhankarmane <onlinestudysam@gmail.com>
2021-08-13 18:16:29 +0530

    Refactored route for managing authors

--- routes/controllers/authorController.js
+++ routes/controllers/authorController.js
@@ -2,17 +2,24 @@ const express = require("express");
 const authorize = require("../../middleware/authorize");
 const authorServices = require("../services/authorServices");
 const router = express.Router();
-
-// Routes
-router.post("/add", authorize, addAuthor);
+const db = require("../../models");
+const lodash = require('lodash');
+const wrapperFactory = require("../../middleware/wrapperFactory");
 
 module.exports = router;
 
-function addAuthor(req, res, next) {
-  authorServices
-    .createAuthor(req.body)
-    .then((author) =>
-      res.json({ message: "Author added successfully", author: author })
-    )
-    .catch(next);
+router.post("/add", authorize, wrapperFactory(async (req, res, next) => {
+
+    const input = getAuthorFromRequest(req);
+    const author = await db.Author.findOrCreate({
+        where: {
+            Name: input.name,
+        },
+    });
+    res.json({message: "Author added successfully", author: author});
+
+}));
+
+function getAuthorFromRequest(req) {
+    return lodash.pick(req.body, ["name"]);
 }
