f9d66d5cfb3def9a49b41972175f763b1d88a2b5
Shubhankar <onlinestudysam@gmail.com>
2021-08-18 14:04:39 +0530

    Updated query in the payment checking middleware for sqlite

--- middleware/checkPayment.js
+++ middleware/checkPayment.js
@@ -5,12 +5,9 @@ const checkPaymentStatus = (req, res, next) => {
 };
 
 async function checkSubscription(customerId) {
-    // const daysSincePayment = await db.sequelize.query('CALL get_days_since_last_payment(:customerId)', {
-    //     replacements: {customerId: customerId}
-    // })
-
-    const daysSincePayment = await prisma.$queryRaw`select (datediff(current_date, lastPaymentDate)) as difference_in_days from customers where id = ${customerId};`;
-
+    // const daysSincePayment = await prisma.$queryRaw`select (datediff(current_date, lastPaymentDate)) as difference_in_days from customers where id = ${customerId};`;
+    const daysSincePayment = await prisma.$queryRaw`select (date('now') - date(round(lastPaymentDate / 1000), 'unixepoch')) as difference_in_days from customers where id = ${customerId};`;
+    // console.log(daysSincePayment);
     if (daysSincePayment[0].difference_in_days === null) {
         throw "Customer not subscribed";
     }
